6dc15322d91defa30a46a73b626a2547f459d0dc
Huawei <Huawei@LAPTOP-Q85GDL68>
2026-03-26 16:37:06 +0300

    Добавьте файлы проекта.

---
+++ WebTechnology/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebTechnology.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
