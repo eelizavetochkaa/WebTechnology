7760a9e148a456f0e36d507039938da780347814
eelizavetochkaa <liza.bogdanova29@gmail.com>
2026-04-16 14:55:24 +0300

    добавила адаптивность

---
+++ WebTechnology/Controllers/CartController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WebTechnology.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebTechnology.Controllers
+{
+    public class CartController : Controller
+    {
+        private const string CartSessionKey = "Cart";
+
+        private List<CartItem> GetCart()
+        {
+            var cart = HttpContext.Session.GetString(CartSessionKey);
+            if (string.IsNullOrEmpty(cart))
+            {
+                return new List<CartItem>();
+            }
+            return JsonConvert.DeserializeObject<List<CartItem>>(cart);
+        }
+        private void SaveCart(List<CartItem> cart)
+        {
+            HttpContext.Session.SetString(CartSessionKey, JsonConvert.SerializeObject(cart));
+        }
+        public IActionResult Index()
+        {
+            var cart = GetCart();
+            return View(cart);
+        }
+        public IActionResult Add(int id, string name, decimal price)
+        {
+            var cart = GetCart();
+            var existingItem = cart.FirstOrDefault(x => x.ProductId == id);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity++;
+            }
+            else
+            {
+                cart.Add(new CartItem
+                {
+                    ProductId = id,
+                    ProductName = name,
+                    Price = price,
+                    Quantity = 1
+                });
+            }
+
+            SaveCart(cart);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Remove(int id)
+        {
+            var cart = GetCart();
+            var item = cart.FirstOrDefault(x => x.ProductId == id);
+            if (item != null)
+            {
+                cart.Remove(item);
+            }
+            SaveCart(cart);
+            return RedirectToAction("Index");
+        }
+        public IActionResult UpdateQuantity(int id, int quantity)
+        {
+            var cart = GetCart();
+            var item = cart.FirstOrDefault(x => x.ProductId == id);
+            if (item != null && quantity > 0)
+            {
+                item.Quantity = quantity;
+            }
+            SaveCart(cart);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Clear()
+        {
+            SaveCart(new List<CartItem>());
+            return RedirectToAction("Index");
+        }
+    }
+}
